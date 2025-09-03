93d140f107594dd4ca95de97988bb3724994013d
Sigurd Løkse <sigl@norceresearch.no>
2025-09-03 14:47:28 +0200

    Ghostty, fish and tmux

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,14 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
+set -x HOMEBREW_PREFIX $HOME/.local
+set -x HOMEBREW_CASK_OPTS "--appdir=$HOME/Applications"
+
+set SSH_AUTH_SOCK $HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
+fish_add_path $HOME'/Applications/Docker.app/Contents/Resources/bin'
+fish_add_path $HOME'.local/bin'
+fish_add_path $HOME'.docker/bin'
+fish_add_path $HOMEBREW_PREFIX'bin'
+
+eval "$(brew shellenv)"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -18,20 +28,13 @@ alias tree="tree -CA"
 alias less="less -R"
 alias printenv="printenv | LC_ALL=C sort"
 
-set -x HOMEBREW_CASK_OPTS "--appdir=~/Applications"
-
-fish_add_path $HOME'/Applications/Docker.app/Contents/Resources/bin'
-
 # Hooks
 zoxide init fish | source
 starship init fish | source
 direnv hook fish | source
 fzf --fish | source
 
-source $HOME/.cargo/env.fish
+#source $HOME/.cargo/env.fish
 
 # Transient prompt (Starship command)
 enable_transience
-
-# Created by `pipx` on 2024-10-01 10:05:19
-set PATH $PATH /Users/sigl/.local/bin
